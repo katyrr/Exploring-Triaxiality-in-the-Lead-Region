c5b94ea0c4525697a7d389103af865d42666b21b
katy <krr511@york.ac.uk>
2025-02-12 18:35:28 +0000

    clean: removing old files, and moving to archive

--- RunGAMPN_1.sh
+++
@@ -1,5 +0,0 @@
-
-(cd Batch1; ./../../../../Executables/MO/gampn < ../../Inputs/GAM_e0.001_g0.2_p0.730_Pb207.DAT)
-cp Batch1/GAMPN.out GAM_e0.001_g0.2_p0.730_Pb207.OUT
-
-echo message from terminal: finished running gampn batch 1
